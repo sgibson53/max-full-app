38188591fa13972544f3de2d9c8a36c73f46a8c7
Seth Gibson <wgibson53@gmail.com>
2018-08-07 08:21:33 -0500

    app setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { MaterialModule } from "./material.module";
+import { SignupComponent } from "./auth/signup/signup.component";
+import { LoginComponent } from "./auth/login/login.component";
+import { TrainingComponent } from "./training/training.component";
+import { CurrentTrainingComponent } from "./training/current-training/current-training.component";
+import { PastTrainingComponent } from "./training/past-training/past-training.component";
+import { WelcomeComponent } from "./welcome/welcome.component";
+import { AppRoutingModule } from "./app-routing.module";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    LoginComponent,
+    TrainingComponent,
+    CurrentTrainingComponent,
+    PastTrainingComponent,
+    WelcomeComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, MaterialModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
